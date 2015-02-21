981b77aae940a494c9ca3b3892fdc923e0b373b9
Carlos Sánchez San Román <carlos.sanchez.sanroman@gmail.com>
2015-02-21 18:09:25 +0100

    Final commit

--- server.R
+++ server.R
@@ -1,17 +1,9 @@
 library(shiny)
-library(UsingR)
-data(galton)
+data(mtcars)
 
 shinyServer(
   function(input, output) {
-    output$myHist <- renderPlot({
-      hist(galton$child, xlab='child height', col='lightblue',main='Histogram')
-      mu <- input$mu
-      lines(c(mu, mu), c(0, 200),col="red",lwd=5)
-      mse <- mean((galton$child - mu)^2)
-      text(63, 150, paste("mu = ", mu))
-      text(63, 140, paste("MSE = ", round(mse, 2)))
-    })
-    
+    fit <- lm(qsec ~ hp + wt, data=mtcars)
+    output$predict <- renderPrint({predict(fit,newdata = data.frame(hp = input$hp, wt = input$wt/1000))})
   }
 )
--- ui.R
+++ ui.R
@@ -1,10 +1,15 @@
 library(shiny)
 shinyUI(pageWithSidebar(
-  headerPanel("Example plot"),
+  headerPanel("How long does it take for the car to travel 1/4 mile?"),
   sidebarPanel(
-    sliderInput('mu', 'Guess at the mu',value = 70, min = 60, max = 80, step = 0.05,)
+    sliderInput('hp', 'Horsepower',value = 200, min = 100, max = 500, step = 10,),
+    sliderInput('wt', 'Weight(lb)',value = 3000, min = 1500, max = 4500, step = 100,)
+   
   ),
   mainPanel(
-    plotOutput('myHist')
+    h3('Time in seconds to travel 1/4 mile'),
+    verbatimTextOutput("predict"),
+    h4('Help'),
+    h6('You must introduce your car\'s horsepower and weight(in pounds) and you\'ll get the expected time your car will take to travel 1/4 mile.')
   )
 ))
